a79242dc9443ce23eb431bc71116ca3fba79af6c
蒋薛红 <3156262083@qq.com>
2025-07-15 15:26:23 +0800

    添加LGBM，刷新结果

--- week4/scripts/configuration.py
+++ week4/scripts/configuration.py
@@ -3,6 +3,7 @@
 from sklearn.ensemble import GradientBoostingRegressor
 from xgboost import XGBRegressor
 from sklearn.model_selection import TimeSeriesSplit
+from lightgbm import LGBMRegressor
 
 conf = {
     "GradientBoosting": {
@@ -23,6 +24,20 @@ conf = {
             "objective": 'reg:squarederror',
             "random_state": 42,
             "n_jobs": -1
+        },
+        "param_grid": {
+            'n_estimators': [100, 200],
+            'learning_rate': [0.05, 0.1],
+            'max_depth': [3, 5],
+            'subsample': [0.8, 1.0]
+        }
+    },
+    "LightGBM": {
+        "model_name": LGBMRegressor,
+        "model_params": {
+            "random_state": 42,
+            "n_jobs": -1,
+            "verbose": -1
         },
         "param_grid": {
             'n_estimators': [100, 200],
