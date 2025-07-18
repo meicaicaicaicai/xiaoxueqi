143b2332f45370aa69e98a2e4626ecd327320126
蒋薛红 <3156262083@qq.com>
2025-07-18 11:12:00 +0800

    删除冲突文件

--- week2/mat_pumpkins.py
+++ week2/mat_pumpkins.py
@@ -1,4 +1,4 @@
-@@ -6,7 +6,6 @@ from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 from statsmodels.tsa.seasonal import seasonal_decompose
